de3f038500bacddfae0d1621cae9b6bc841f7ff8
Kristian Nese <kristian.nese@microsoft.com>
2020-01-15 16:03:36 +0100

    Create New-AzLandingZoneSubscription.ps1

---
+++ src/New-AzLandingZoneSubscription.ps1
@@ -0,0 +1,18 @@
+# PowerShell sample to create new Azure Subscriptions and move to target management group
+param (
+    $subscriptionDisplayName,
+    $targetManagementGroupId,
+    $offerType = "MS-AZR-0017P"
+)
+                        
+    Write-Host "Creating new subscription"
+
+    $subscription = New-AzSubscription -Name $subscriptionDisplayName `
+                                        -OfferType $offerType `
+                                        -EnrollmentAccountObjectId (Get-AzureRmEnrollmentAccount)[0].ObjectId
+
+    Write-Host "Creating new subscription Success!"
+
+
+    # Assign Subscription to its Management Group .
+    New-AzManagementGroupSubscription -GroupName $ManagementGroupName -SubscriptionId $subscription.SubscriptionId
